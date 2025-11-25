e1e8af80d142196ddc95447d1b6188ba9bac5195
aranyaMUT <6811130004@mut.ac.th>
2025-11-25 17:28:22 +0700

    Test01

---
+++ LAB01/int.cpp
@@ -0,0 +1,10 @@
+// test01.cpp
+
+#include <iostream>
+
+int main() {
+    std::cout << "- Name: Aranya Jehmin" << std::endl;
+    std::cout << "- Course: MIIA106" << std::endl;
+    std::cout << "- Email: 6811130004@mut.ac.th" << std::endl;
+    return 0;
+}
